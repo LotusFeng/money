37b14b5d947f392f1217f3ecb381f252ef93ee34
czf <505487452@qq.com>
2022-09-23 15:30:57 +0800

    0.1

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>momey
+  <div class="nav-wrapper">
+    <div class="content">
+      momey
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,5 +14,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    overflow: auto;
+    flex-grow: 1;
+  }
 
 </style>
