2ca6189b69be3de1e2c3774f9e53a994b202491d
czf <505487452@qq.com>
2022-09-27 11:26:26 +0800

    svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:herf="#Momey"/>
+      </svg>
+      记账
+    </router-link>
     |
     <router-link to="/labels">标签</router-link>
     |
@@ -9,6 +14,10 @@
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/Money.svg';
+
+console.log(x);
+
 export default {
   name: 'Nav'
 };
