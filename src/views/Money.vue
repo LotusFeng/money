0c91bffc557d9c7d41a99e17700bd75ae0715f2d
czf <505487452@qq.com>
2022-10-14 11:34:10 +0800

    NumberPad.vue基本完成

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad/>
-    <Types xxx="hi"/>
+    <Types/>
     <Notes/>
     <Tags/>
   </Layout>
@@ -12,6 +12,7 @@ import NumberPad from '@/components/money/NumberPad.vue';
 import Types from '@/components/money/Types.vue';
 import Notes from '@/components/money/Notes.vue';
 import Tags from '@/components/money/Tags.vue';
+
 export default {
   name: 'Money',
   components: {Tags, Notes, Types, NumberPad},
