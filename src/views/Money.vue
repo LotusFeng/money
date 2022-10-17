ebe22003ed9e2a2a4522eec8a4b62d22343c886d
czf <505487452@qq.com>
2022-10-17 11:12:55 +0800

    Tags可以选中取消

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,20 +3,25 @@
     <NumberPad/>
     <Types/>
     <Notes/>
-    <Tags/>
+    <Tags :data-source="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
-import NumberPad from '@/components/money/NumberPad.vue';
-import Types from '@/components/money/Types.vue';
-import Notes from '@/components/money/Notes.vue';
-import Tags from '@/components/money/Tags.vue';
+<script>
+import NumberPad from '@/components/money/NumberPad.vue'
+import Types from '@/components/money/Types.vue'
+import Notes from '@/components/money/Notes.vue'
+import Tags from '@/components/money/Tags.vue'
 
 export default {
   name: 'Money',
   components: {Tags, Notes, Types, NumberPad},
-};
+  data() {
+    return{
+      tags: ['衣', '食', '住', '行']
+    }
+  }
+}
 </script>
 
 <style lang="scss">
