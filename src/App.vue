bc8632c67412d194fb5e7bfef36ab66692338f44
czf <505487452@qq.com>
2022-10-31 10:17:47 +0800

    优化pc

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
 <style lang="scss">
 @import "~@/assets/style/helper.scss";
 @import "~@/assets/style/reset.scss";
-body{
+
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
@@ -15,5 +16,10 @@ body{
   font-size: 16px;
   line-height: 1.5;
   background: #f5f5f5;
+}
+
+#div {
+  max-width: 500px;
+  margin: 0 auto;
 }
 </style>
